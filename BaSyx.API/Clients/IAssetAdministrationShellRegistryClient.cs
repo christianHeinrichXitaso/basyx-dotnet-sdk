49c3f0f444fb8f6eb9040e01fc52a819c4df04db
Constantin Ziesche <constantin.ziesche@bosch.com>
2022-03-16 00:34:01 +0100

    Added async methods

--- BaSyx.API/Clients/IAssetAdministrationShellRegistryClient.cs
+++ BaSyx.API/Clients/IAssetAdministrationShellRegistryClient.cs
@@ -9,11 +9,50 @@
 * SPDX-License-Identifier: MIT
 *******************************************************************************/
 using BaSyx.API.Interfaces;
+using BaSyx.Models.AdminShell;
+using BaSyx.Models.Connectivity;
+using BaSyx.Utils.ResultHandling;
+using System;
+using System.Threading.Tasks;
 
 namespace BaSyx.API.Clients
 {
     public interface IAssetAdministrationShellRegistryClient : IAssetAdministrationShellRegistryInterface
     {
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.CreateAssetAdministrationShellRegistration(IAssetAdministrationShellDescriptor)"/>
+        Task<IResult<IAssetAdministrationShellDescriptor>> CreateAssetAdministrationShellRegistrationAsync(IAssetAdministrationShellDescriptor aasDescriptor);
 
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.UpdateAssetAdministrationShellRegistration(string, IAssetAdministrationShellDescriptor)"/>
+        Task<IResult<IAssetAdministrationShellDescriptor>> UpdateAssetAdministrationShellRegistrationAsync(string aasIdentifier, IAssetAdministrationShellDescriptor aasDescriptor);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.RetrieveAssetAdministrationShellRegistration(string)"/>
+        Task<IResult<IAssetAdministrationShellDescriptor>> RetrieveAssetAdministrationShellRegistrationAsync(string aasIdentifier);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.RetrieveAllAssetAdministrationShellRegistrations"/>
+        Task<IResult<IQueryableElementContainer<IAssetAdministrationShellDescriptor>>> RetrieveAllAssetAdministrationShellRegistrationsAsync();
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.RetrieveAllAssetAdministrationShellRegistrations(Predicate{IAssetAdministrationShellDescriptor})"/>
+        Task<IResult<IQueryableElementContainer<IAssetAdministrationShellDescriptor>>> RetrieveAllAssetAdministrationShellRegistrationsAsync(Predicate<IAssetAdministrationShellDescriptor> predicate);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.DeleteAssetAdministrationShellRegistration(string)"/>
+        Task<IResult> DeleteAssetAdministrationShellRegistrationAsync(string aasIdentifier);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.CreateSubmodelRegistration(string, ISubmodelDescriptor)"/>
+        Task<IResult<ISubmodelDescriptor>> CreateSubmodelRegistrationAsync(string aasIdentifier, ISubmodelDescriptor submodelDescriptor);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.UpdateSubmodelRegistration(string, string, ISubmodelDescriptor)"/>
+        Task<IResult<ISubmodelDescriptor>> UpdateSubmodelRegistrationAsync(string aasIdentifier, string submodelIdentifier, ISubmodelDescriptor submodelDescriptor);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.RetrieveAllSubmodelRegistrations(string)"/>
+        Task<IResult<IQueryableElementContainer<ISubmodelDescriptor>>> RetrieveAllSubmodelRegistrationsAsync(string aasIdentifier);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.RetrieveAllSubmodelRegistrations(string, Predicate{ISubmodelDescriptor})"/>
+        Task<IResult<IQueryableElementContainer<ISubmodelDescriptor>>> RetrieveAllSubmodelRegistrationsAsync(string aasIdentifier, Predicate<ISubmodelDescriptor> predicate);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.RetrieveSubmodelRegistration(string, string)"/>
+        Task<IResult<ISubmodelDescriptor>> RetrieveSubmodelRegistrationAsync(string aasIdentifier, string submodelIdentifier);
+
+        ///<inheritdoc cref="IAssetAdministrationShellRegistryInterface.DeleteSubmodelRegistration(string, string)"/>
+        Task<IResult> DeleteSubmodelRegistrationAsync(string aasIdentifier, string submodelIdentifier);
     }
 }
